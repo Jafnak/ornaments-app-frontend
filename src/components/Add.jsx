78e7a1f4c53f2fc2f69fe4315f9932b023b2c503
Jafnak <jafnakusman@gmail.com>
2024-06-12 22:35:25 +0530

    add updated

---
+++ src/components/Add.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Add = () => {
+  return (
+    <div>
+
+<div className="container">
+    <div className="row g-3">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <label htmlFor="" className="formlabel">Ornament name</label>
+            <input type="text" className="form-control" />
+        </div>
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <label htmlFor="" className="form-label">Description</label>
+            <input type="text" className="form-control" />
+        </div>
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <label htmlFor="" className="form-label">Price</label>
+            <input htmlFor="text" className="form-control"/>
+        </div>
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <button className="btn btn-primary">Add</button>
+        </div>
+    </div>
+</div>
+
+    </div>
+
+    
+  )
+}
+
+export default Add
